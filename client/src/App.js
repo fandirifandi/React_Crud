c0d1bb07a5cd44fdfab5eea2591d7665a6b67828
fandirifandi <fandirifandi30@gmail.com>
2021-09-27 14:38:16 +0700

    new

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,94 @@
+import { useState } from 'react';
 import './App.css';
+import Axios from 'axios';
 
 function App() {
+
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [country, setCountry] = useState("");
+  const [position, setPosition] = useState("");
+  const [wage, setWage] = useState(0);
+
+  
+  const [employeeList, setemployeeList] = useState([]);
+
+  const addEmployee = () => {
+    Axios.post('http://localhost:3001/create', {
+      name: name,
+      age: age,
+      country: country,
+      position: position,
+      wage: wage,
+    }).then(() => {
+      console.log("success")
+    });
+  };
+
+  const getEmployees = () => {
+    Axios.get('http://localhost:3001/employees').then((response) => {
+      setemployeeList(response.data);
+    });
+  }
+
+  const deleteEmployees = () => {
+    Axios.delete('http://localhost:3001/delete/$employeeList');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className="App">
+    <div className="Information">
+      <label>Name:</label>
+      <input 
+        type="text" 
+          onChange={(event) =>{
+            setName(event.target.value);
+          }}
+      />
+      <label> Age: </label>
+      <input 
+          type="number"
+            onChange={(event) =>{
+              setAge(event.target.value);
+            }}
+      />
+      <label>Country</label>
+      <input 
+        type="text"  
+          onChange={(event) =>{
+              setCountry(event.target.value);
+            }}  
+      />
+      <label>Position</label>
+      <input 
+        type="text"  
+          onChange={(event) =>{
+              setPosition(event.target.value);
+            }}
+      />
+      <label>Wage:</label>
+      <input 
+        type="number"  
+          onChange={(event) =>{
+              setWage(event.target.value);
+            }}
+       />
+      <button onClick={addEmployee}> Add Employee </button>
     </div>
-  );
+    <div className="employees">
+    <button onClick={getEmployees}> Show Employees </button>
+    {employeeList.map((val,key) => {
+      return <div className="employee"> 
+      <h3>Name : {val.name}</h3>
+      <h3>Age : {val.age}</h3> 
+      <h3>Country :{val.country}</h3> 
+      <h3>Position : {val.position}</h3> 
+      <h3>Wage : {val.wage}</h3> 
+      </div>;
+    })}
+    </div>
+
+  </div> );
 }
 
 export default App;
